e977c2b3e8f1379536ffb733599fdbeed90af6a6
Kevin Roberts <kevin@shoutly.com>
2024-10-11 16:06:45 +0200

    Update cases

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { UserFormComponent } from "./app/components/user-form/user-form.component";
+import { TaskDescriptionComponent } from "./app/components/task-description/task-description.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   template: `
     <app-user-form></app-user-form>
+    <app-task-description></app-task-description>
   `,
-  imports: [UserFormComponent],
+  imports: [UserFormComponent, TaskDescriptionComponent],
 })
 export class App {
   name = 'Angular';
