efd24d09e30655f90efa0af4c5680e0e01917118
ShakerNasser <shaker.nasser@outlook.com>
2024-10-15 00:16:48 +0200

    skills added usermodel

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -5,6 +5,7 @@ export interface UserBackend {
     last_name: string;
     age: number | null;
     email: string;
+    skills: string[]; // Extend Functionality: Added skills property to user.model
   }
   
   export interface User extends UserBackend {
